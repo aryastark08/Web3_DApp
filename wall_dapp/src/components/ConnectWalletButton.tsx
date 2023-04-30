15caa5364587114f7e26752da49761ddc0be058e
Arya <aishwaryanair430@gmail.com>
2023-04-30 23:29:04 +0530

    working on further elements

--- wall_dapp/src/components/ConnectWalletButton.tsx
+++ wall_dapp/src/components/ConnectWalletButton.tsx
@@ -1,55 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { Button, Icon } from "@chakra-ui/react";
-import { MdWallet } from "react-icons/md";
-import { useWeb3React } from "@web3-react/core";
-import { Web3Provider } from '@ethersproject/providers'
-import { injected } from "../connectors";
-import Swapper from "./Swapper";
+// import React, { useState, useEffect } from "react";
+// import { Button, Icon } from "@chakra-ui/react";
+// import { MdWallet } from "react-icons/md";
+// import { useWeb3React } from "@web3-react/core";
+// import { Web3Provider } from '@ethersproject/providers'
+// import { injected } from "../connectors";
+// import Swapper from "./Swapper";
 
 
-const ConnectWalletButton = () => {
-    const [walletConnected, setWalletConnected] = useState(false);
-    const { context, account, activate } = useWeb3React<Web3Provider>();
+// const ConnectWalletButton = () => {
+//     const [walletConnected, setWalletConnected] = useState(false);
+//     const { context, account, activate } = useWeb3React<Web3Provider>();
   
-    useEffect(() => {
-      if (context?.active && account) {
-        setWalletConnected(true);
-      } else {
-        setWalletConnected(false);
-      }
-    }, [context, account]);
+//     useEffect(() => {
+//       if (context?.active && account) {
+//         setWalletConnected(true);
+//       } else {
+//         setWalletConnected(false);
+//       }
+//     }, [context, account]);
   
-    const handleConnectWallet = async () => {
-      try {
-        await activate(injected);
-      } catch (ex) {
-        console.error(ex);
-      }
-    };
+//     const handleConnectWallet = async () => {
+//       try {
+//         await activate(injected);
+//       } catch (ex) {
+//         console.error(ex);
+//       }
+//     };
   
-    const connectWalletPrompt = async () => {
-      try {
-        await activate(injected, undefined, true);
-      } catch (ex) {
-        console.error(ex);
-      }
-    };
+//     const connectWalletPrompt = async () => {
+//       try {
+//         await activate(injected, undefined, true);
+//       } catch (ex) {
+//         console.error(ex);
+//       }
+//     };
   
-    return (
-      <>
-        {!walletConnected ? (
-          <Button
-            leftIcon={<Icon as={MdWallet} color="currentColor" size="24px" />}
-            variant="outline"
-            onClick={handleConnectWallet}
-          >
-            Connect Wallet
-          </Button>
-        ) : (
-          <Swapper />
-        )}
-      </>
-    );
-  };
+//     return (
+//       <>
+//         {!walletConnected ? (
+//           <Button
+//             leftIcon={<Icon as={MdWallet} color="currentColor" size="24px" />}
+//             variant="outline"
+//             onClick={handleConnectWallet}
+//           >
+//             Connect Wallet
+//           </Button>
+//         ) : (
+//           <Swapper />
+//         )}
+//       </>
+//     );
+//   };
   
-  export default ConnectWalletButton;
+//   export default ConnectWalletButton;
